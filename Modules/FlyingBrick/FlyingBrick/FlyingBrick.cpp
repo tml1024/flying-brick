1c9c75f629eb78037d8f35bb4847d9d2c6e8e349
Tor Lillqvist <tml@iki.fi>
2021-05-07 17:44:00 +0300

    Add a VS solution to build a standalone Wasm module

---
+++ Modules/FlyingBrick/FlyingBrick/FlyingBrick.cpp
@@ -0,0 +1,26 @@
+﻿#include "MSFS/MSFS.h"
+
+#include "FlyingBrick.h"
+#include <iostream>
+#include <fstream>
+
+extern "C" MODULE_EXPORT void test(void)
+{
+	// TODO
+}
+
+extern "C" MSFS_CALLBACK void module_init(void)
+{
+	std::ofstream log("\\work\\FlyingBrick.wasm.log", std::ios_base::app);
+
+	log << "Hello there, module_init here." << std::endl;
+
+	std::cout << "====== FlyingBrick: Let's see if writing to stdout goes anywhere" << std::endl;
+}
+
+extern "C" MSFS_CALLBACK void module_deinit(void)
+{
+	std::ofstream log("\\work\\FlyingBrick.wasm.log", std::ios_base::app);
+
+	log << "Hello there, module_deinit here." << std::endl;
+}
